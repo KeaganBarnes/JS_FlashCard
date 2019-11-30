7b43090836c1939336388e4fccb4832c28c8ff15
Keagan Barnes <barnes.keagan9@gmail.com>
2019-11-30 15:34:14 -0700

    added second html file, cant seem to push into my array

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const cards = document.querySelectorAll('.flashcard');
+
+document.getElementById('form').addEventListener('Display Answer',(e)=>{
+  let question = document.getElementById('question').value;
+  let answer = document.getElementById('answer').value;
+
+  createCards(question, answer);
+  (e).preventDefault();
+});
+
+var cards= [];
+// push method won't push question-answer key value pairs into array
+function createCards(question, answer) {
+  var card = {
+      question:question,
+      answer:answer
+}
+    cards.push(card);
+    console.log(cards);
+    readCards();
+    document.getElementById('form').reset();
+  };
 
 
 function flipCard() {
